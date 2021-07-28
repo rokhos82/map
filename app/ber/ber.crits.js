95a19f42cd2e3d89a6f19471a2fb8b43efac0efc
Justin Lane <justin.lane@conversecountywy.gov>
2021-07-28 15:53:35 -0600

    2021-07-28 - Crit Table File

---
+++ app/ber/ber.crits.js
@@ -0,0 +1,11 @@
+export const generalTable = [
+  { msg:"Reactor Core Breach (Ship Explodes)" },
+  { msg:"Structural Collapse (+3 Damage)" },
+  { msg:"Structural Collapse (+3 Damage)" },
+  { msg:"Explosion Amidships (+2 damage)" },
+  { msg:"Explosion Amidships (+2 damage)" },
+  { msg:"Superstructure Hit (+1 damage)" },
+  { msg:"Superstructure Hit (+1 damage)" },
+  { msg:"Inertial Dampeners Down (+1 damage)" },
+  { msg:"Inertial Dampeners Down (+1 damage)" }
+];
